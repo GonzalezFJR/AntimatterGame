198ca6457b4790336e1b1dd9e85e0aa7cdec0741
Xuan <jrgonzal@cern.ch>
2020-11-24 13:22:02 +0100

    Modifying colors

--- plotter/antimatterAnimation.py
+++ plotter/antimatterAnimation.py
@@ -16,13 +16,22 @@ from physics.particle import *
 from matplotlib.widgets import Slider, Button, RadioButtons
 from matplotlib.text import Text
 
-axcolor = 'lightgoldenrodyellow'
+axcolor = 'aliceblue'
+sliderColor = 'indigo'#'dodgerblue'
+textcolor = 'w'
+inTextColor = 'k'
+bkgcolor = 'k'
+electronColor = 'green'
+positronColor = 'red'
+photonColor = 'w'
+borderColor = 'w'
 
+partcolor = {'electron':electronColor, 'positron':positronColor, 'photon':photonColor}
 
 class antimatterAnimation:
 
   def __init__(self, universe, width=4, savename=None):
-    self.fig, self.ax = plt.subplots()
+    self.fig, self.ax = plt.subplots(facecolor=bkgcolor)
     plt.subplots_adjust(left=0.1, bottom=0.3, right=0.9, top=0.9)
     self.universe = universe
     self.text = ''
@@ -31,39 +40,72 @@ class antimatterAnimation:
     self.partmode = 'electron'
     self.savename = savename
     for p in self.universe.GetParticles(): 
-      self.plots.append(self.ax.plot([], [],'o',color = p.color, fillstyle='none' if p.color=='k' else 'full'))
+      col = partcolor[p.name]
+      self.plots.append(self.ax.plot([], [],'o',color = col, fillstyle='none' if p.name=='photon' else 'full'))
     self.width = width
     self.height= self.width
     self.randomSpeedModule = 0.5
+
+    self.ax.set_facecolor(bkgcolor)
+    self.ax.spines['bottom'].set_color(borderColor)
+    self.ax.spines['top']   .set_color(borderColor)
+    self.ax.spines['left']  .set_color(borderColor)
+    self.ax.spines['right'] .set_color(borderColor)
+    self.ax.xaxis.label.set_color(bkgcolor)
+    self.ax.yaxis.label.set_color(bkgcolor)
+    self.ax.tick_params(axis='x', colors=bkgcolor)
+    self.ax.tick_params(axis='y', colors=bkgcolor)
     
     resetax = plt.axes([0.77, 0.025, 0.14, 0.04])
+    for l in ['bottom', 'top', 'left', 'right']: resetax.spines[l].set_color(borderColor)
     self.button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+    self.button.label.set_color(inTextColor)
 
-    self.rmtax    = plt.axes([0.62, 0.025, 0.14, 0.04])
-    self.rmbutton = Button(self.rmtax, 'Borders OFF', color=axcolor, hovercolor='0.975')
+    rmtax    = plt.axes([0.62, 0.025, 0.14, 0.04])
+    for l in ['bottom', 'top', 'left', 'right']: rmtax.spines[l].set_color(borderColor)
+    self.rmbutton = Button(rmtax, 'Borders OFF', color=axcolor, hovercolor='0.975')
+    self.rmbutton.label.set_color(inTextColor)
     
     rax = plt.axes([0.77, 0.075, 0.14, 0.16], facecolor=axcolor)
-    self.particleSelector = RadioButtons(rax, ('electron', 'positron'), active=0)
+    for l in ['bottom', 'top', 'left', 'right']: rax.spines[l].set_color(borderColor)
+    self.particleSelector = RadioButtons(rax, ('electron', 'positron'), active=0, activecolor=sliderColor)
+    #self.particleSelector.label.set_color(inTextColor)
 
     createax  = plt.axes([0.62, 0.160, 0.14, 0.075], facecolor=axcolor)
+    for l in ['bottom', 'top', 'left', 'right']: createax.spines[l].set_color(borderColor)
     self.createRandomPartButtom  = Button(createax, 'Create 25\nparticles', color=axcolor)
+    self.createRandomPartButtom.label.set_color(inTextColor)
     createax2 = plt.axes([0.62, 0.075, 0.14, 0.075], facecolor=axcolor)
+    for l in ['bottom', 'top', 'left', 'right']: createax2.spines[l].set_color(borderColor)
     self.createRandomPartButtom2 = Button(createax2, 'Create 25\nantiparticles', color=axcolor)
+    self.createRandomPartButtom2.label.set_color(inTextColor)
 
     axmag   = plt.axes([0.20, 0.05, 0.35, 0.03], facecolor=axcolor)
     axelecX = plt.axes([0.20, 0.11, 0.35, 0.03], facecolor=axcolor)
     axelecY = plt.axes([0.20, 0.17, 0.35, 0.03], facecolor=axcolor)
+    for l in ['bottom', 'top', 'left', 'right']: axmag.spines[l].set_color(borderColor)
+    for l in ['bottom', 'top', 'left', 'right']: axelecX.spines[l].set_color(borderColor)
+    for l in ['bottom', 'top', 'left', 'right']: axelecY.spines[l].set_color(borderColor)
     bx,by,bz = self.universe.magneticField.Get()
     ex,ey,ez = self.universe.electricField.Get()
     maxB = self.width/2
     maxE = self.width/2
 
-    self.magFieldSlider   = Slider(axmag,  'Magnetic field', -maxB, maxB, valinit=0)#bz if abs(bz)<maxB else 0)#, valstep=delta_f)
-    self.elecFieldSliderX = Slider(axelecX, 'Electric field (X)', -maxE, maxE, valinit=0)#ex if abs(ex)<maxE else 0)
-    self.elecFieldSliderY = Slider(axelecY, 'Electric field (Y)', -maxE, maxE, valinit=0)#ey if abs(ey)<maxE else 0)
+    self.magFieldSlider   = Slider(axmag,  'Magnetic field', -maxB, maxB, valinit=0, color=sliderColor, valfmt='%1.2f')#bz if abs(bz)<maxB else 0)#, valstep=delta_f)
+    self.elecFieldSliderX = Slider(axelecX, 'Electric field (X)', -maxE, maxE, valinit=0, color=sliderColor, valfmt='%1.2f')#ex if abs(ex)<maxE else 0)
+    self.elecFieldSliderY = Slider(axelecY, 'Electric field (Y)', -maxE, maxE, valinit=0, color=sliderColor, valfmt='%1.2f')#ey if abs(ey)<maxE else 0)
+    self.magFieldSlider.label.set_color(textcolor)
+    self.magFieldSlider.valtext.set_color(textcolor)
+    self.elecFieldSliderX.label.set_color(textcolor)
+    self.elecFieldSliderX.valtext.set_color(textcolor)
+    self.elecFieldSliderY.label.set_color(textcolor)
+    self.elecFieldSliderY.valtext.set_color(textcolor)
 
     axrndmspeed = plt.axes([0.93, 0.50, 0.03, 0.15], facecolor=axcolor)
-    self.randomSpeedSlider = Slider(axrndmspeed, 'Particle\nspeed', 0.1, 1, valinit=0.5, orientation='vertical')
+    for l in ['bottom', 'top', 'left', 'right']: axrndmspeed.spines[l].set_color(borderColor)
+    self.randomSpeedSlider = Slider(axrndmspeed, 'Particle\nspeed', 0.1, 1, valinit=0.5, orientation='vertical', color=sliderColor)
+    self.randomSpeedSlider.label.set_color(textcolor)
+    self.randomSpeedSlider.valtext.set_color(textcolor)
     
   ################################################
   ################################################
@@ -77,10 +119,10 @@ class antimatterAnimation:
     prevPart = len(self.plots)
     for p in self.universe.GetParticles():
       if ipart >= prevPart: 
-        self.plots.append(self.ax.plot([], [],'o',color = p.color, fillstyle=('none' if p.color=='k' else 'full') ))
+        self.plots.append(self.ax.plot([], [],'o',color = partcolor[p.name], fillstyle=('none' if p.name=='photon' else 'full') ))
       # Set color and style
-      self.plots[ipart][0].set_color(p.color)
-      if p.color == 'k': self.plots[ipart][0].set_fillstyle('none')
+      self.plots[ipart][0].set_color(partcolor[p.name])
+      if p.name == 'photon': self.plots[ipart][0].set_fillstyle('none')
       else:              self.plots[ipart][0].set_fillstyle('full')
       # Set coordinates
       self.plots[ipart][0].set_data(p.x, p.y); 
@@ -99,10 +141,11 @@ class antimatterAnimation:
     self.ax.set(xlabel='x', ylabel='y', title='', xlim=(-5,5), ylim=(-5,5))
     self.ax.set_ylim(-self.width, self.width)
     self.ax.set_xlim(-self.height,self.height)
-    self.ax.plot([],[],'ob', label='electron')
-    self.ax.plot([],[],'or', label='positron')
-    self.ax.plot([],[],'ok', label='photon', fillstyle='none')
-    self.leg = self.ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncol=3, mode="expand", borderaxespad=0., framealpha=0.8)
+    self.ax.plot([],[],'o', color=electronColor, label='electron')
+    self.ax.plot([],[],'o', color=positronColor, label='positron')
+    self.ax.plot([],[],'o', color=photonColor, label='photon', fillstyle='none')
+    self.leg = self.ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncol=3, mode="expand", borderaxespad=0., framealpha=0.8, facecolor=bkgcolor, edgecolor=borderColor)
+    for t in self.leg.get_texts(): t.set_color(textcolor)
     #self.text1 = self.ax.text(-self.width+2*self.width*0.21, self.height+0.28,'[100]')#, transform=spl.transAxes)
 
     def init():
